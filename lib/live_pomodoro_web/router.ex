f7802a3717585a9a697394a6e23ed177f48d3fee
Santiago <sgobotta@gmail.com>
2024-09-19 00:42:25 -0300

    Add authentication

--- lib/live_pomodoro_web/router.ex
+++ lib/live_pomodoro_web/router.ex
@@ -1,6 +1,8 @@
 defmodule LivePomodoroWeb.Router do
   use LivePomodoroWeb, :router
 
+  import LivePomodoroWeb.UserAuth
+
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
@@ -8,6 +10,7 @@ defmodule LivePomodoroWeb.Router do
     plug :put_root_layout, html: {LivePomodoroWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug :fetch_current_user
   end
 
   pipeline :api do
@@ -39,6 +42,56 @@ defmodule LivePomodoroWeb.Router do
 
       live_dashboard "/dashboard", metrics: LivePomodoroWeb.Telemetry
       forward "/mailbox", Plug.Swoosh.MailboxPreview
+    end
+  end
+
+  ## Authentication routes
+
+  scope "/", LivePomodoroWeb do
+    pipe_through [:browser, :redirect_if_user_is_authenticated]
+
+    live_session :redirect_if_user_is_authenticated,
+      on_mount: [
+        {LivePomodoroWeb.UserAuth, :redirect_if_user_is_authenticated},
+        {LivePomodoroWeb.Theme, :fetch_theme}
+      ] do
+      live "/users/register", UserRegistrationLive, :new
+      live "/users/log_in", UserLoginLive, :new
+      live "/users/reset_password", UserForgotPasswordLive, :new
+      live "/users/reset_password/:token", UserResetPasswordLive, :edit
+    end
+
+    post "/users/log_in", UserSessionController, :create
+  end
+
+  scope "/", LivePomodoroWeb do
+    pipe_through [:browser, :require_authenticated_user]
+
+    live_session :require_authenticated_user,
+      on_mount: [
+        {LivePomodoroWeb.UserAuth, :ensure_authenticated},
+        {LivePomodoroWeb.Theme, :fetch_theme}
+      ] do
+      live "/users/settings", UserSettingsLive, :edit
+
+      live "/users/settings/confirm_email/:token",
+           UserSettingsLive,
+           :confirm_email
+    end
+  end
+
+  scope "/", LivePomodoroWeb do
+    pipe_through [:browser]
+
+    delete "/users/log_out", UserSessionController, :delete
+
+    live_session :current_user,
+      on_mount: [
+        {LivePomodoroWeb.UserAuth, :mount_current_user},
+        {LivePomodoroWeb.Theme, :fetch_theme}
+      ] do
+      live "/users/confirm/:token", UserConfirmationLive, :edit
+      live "/users/confirm", UserConfirmationInstructionsLive, :new
     end
   end
 end
